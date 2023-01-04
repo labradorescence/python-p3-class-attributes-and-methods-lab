56228501435376bc244f1f114e343837993f6a81
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-01-04 14:50:05 -0500

    practice

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,26 @@
 class Song:
+
+    all = []
+
+    count = 0
+
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_to_all(self)
+
+
+    @classmethod
+    def add_to_all(cls, this_song):
+        cls.all.append(this_song)
+
+    @classmethod
+    def add_song_to_count(cls, plus_one=1):
+        cls.count += plus_one
+
+
+
+    
     pass
